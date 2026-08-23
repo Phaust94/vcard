0e3c43492712d7ef1f0139c28891e0524e7832da
phaust94 <mastakovsergei@gmail.com>
2026-08-23 22:31:48 +0200

    Disable cookies

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
   defaults: '2025-05-24',
   capture_pageview: true,
   capture_pageleave: true,
+  cookieless_mode: 'always',             // Prevents setting cookies/local storage
+  person_profiles: 'never',             // Disables user identification features
 })
 
 createRoot(document.getElementById('root')).render(
